57f83f97ad7f561478f91907a78ec835466e6a65
KevinIgnatowicz <52082423+KevinIgnatowicz@users.noreply.github.com>
2019-12-04 18:04:34 -0500

    Link with UQLab

--- post_process_h.m
+++ post_process_h.m
@@ -1,4 +1,6 @@
-function post_process_h(a,n)   %HAX : a = 1 ; HKC : a = 2, n nombre de simulations du lot; executer dans BBDesign
+function post_process_h(a,n, type)   %HAX : a = 1 ; HKC : a = 2, n nombre de simulations du lot; moyenne : type = 1, max : type = 2, position max : type = 3,
+% local : type = 4; executer dans BBDesign
+
 
 if a  == 1 
         
@@ -8,22 +10,22 @@ cd HAX
 mkdir h_moy
 H_MOY = zeros(n,1);
 nom_moy = 'hc_moy';
-nom_moy_txt = strcat(nom_moy, '.txt');
+nom_moy_txt = strcat(nom_moy, '.csv');
 
 mkdir h_max
 H_MAX = zeros(n,1);
 nom_max = 'hc_max';
-nom_max_txt = strcat(nom_max, '.txt');
+nom_max_txt = strcat(nom_max, '.csv');
 
 mkdir position_max
 POS_MAX = zeros(n,1);
 nom_pos = 'pos_max';
-nom_pos_txt = strcat(nom_pos, '.txt');
+nom_pos_txt = strcat(nom_pos, '.csv');
 
 mkdir h_local
 H_LOCAL = zeros(n,113);
 nom_local = 'hc_local';
-nom_local_txt = strcat(nom_local, '.txt');
+nom_local_txt = strcat(nom_local, '.csv');
 
 cd surface_flow_data_hax
 
@@ -41,7 +43,7 @@ for i = 3:size(DIR,1)
     
     Surface_flow = table2array(DATA);
     
-    X= Surface_flow(:,1);
+    X_coord= Surface_flow(:,1);
     HF = Surface_flow(:,15);
    
   cd ..
@@ -62,7 +64,7 @@ for i = 3:size(DIR,1)
     
     H_MOY(i-2,1) = mean(hc(:,1));
    [H_MAX(i-2,1), I] = max(hc(:,1));
-    POS_MAX(i-2,1) = X(I);
+    POS_MAX(i-2,1) = X_coord(I);
     H_LOCAL(i-2,:) = hc';
 cd ..
 cd surface_flow_data_hax
@@ -98,6 +100,13 @@ eval([nom_local '= H_LOCAL']);
         fid = fopen(nom_local_txt, 'wt');
         fwrite(fid, output);
         fclose(fid);
+cd ..
+cd Input
+K = xlsread('K_HAX.xlsx');
+Ratio = xlsread('RATIO_HAX.xlsx');
+Scorr = xlsread('Scorr_HAX.xlsx');
+
+X = [Ratio(1:13) Scorr(1:13)];
 cd ..
 cd ..
 end
@@ -144,7 +153,7 @@ for i = 3:size(DIR,1)
     
     Surface_flow = table2array(DATA);
     
-    X= Surface_flow(:,1);
+    X_coord= Surface_flow(:,1);
     HF = Surface_flow(:,15);
    
   cd ..
@@ -165,7 +174,7 @@ for i = 3:size(DIR,1)
     
     H_MOY(i-2,1) = mean(hc(:,1));
    [H_MAX(i-2,1), I] = max(hc(:,1));
-    POS_MAX(i-2,1) = X(I);
+    POS_MAX(i-2,1) = X_coord(I);
     H_LOCAL(i-2,:) = hc';
 cd ..
 cd surface_flow_data_hkc
@@ -202,6 +211,25 @@ eval([nom_local '= H_LOCAL']);
         fwrite(fid, output);
         fclose(fid);
 cd ..
+cd Input
+K = xlsread('K_HKC.xlsx');
+Ratio = xlsread('RATIO_HKC.xlsx');
+
+X = [K Ratio];
+
+cd ..
 cd ..
 end
+
+if type == 1
+    
+    Y = H_MOY(:,1);
+end
+if type ==2
+    Y = H_MAX(:,1);
+end
+if type == 3
+    Y = POS_MAX(:,1);
+end
+  Metamodel_Sobol_BBD(X, Y, 1)  
 end
