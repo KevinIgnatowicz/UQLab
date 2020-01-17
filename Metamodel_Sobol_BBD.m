abbdee64c8a00b142534de0185afc44bafb701e4
KevinIgnatowicz <52082423+KevinIgnatowicz@users.noreply.github.com>
2020-01-17 14:31:48 -0500

    FullPCE

--- Metamodel_Sobol_BBD.m
+++ Metamodel_Sobol_BBD.m
@@ -8,9 +8,12 @@ MetaOpts.ExpDesign.X = X;
 MetaOpts.ExpDesign.Y = Y;
 MetaOpts.Type = 'Metamodel';
 
+MetaOpts.MetaType = 'PCE';
+MetaOpts.Degree = 1:5;
+%{
 if num == 1 
-MetaOpts.MetaType = 'Kriging';
-MetaOpts.Trend.Type = 'polynomial';
+MetaOpts.MetaType = 'PCK';
+MetaOpts.Trend.Type = 'sequential';
 MetaOpts.Trend.Degree = 2;
 %MetaOpts.Trend.CustomF = [3 2];
 %MetaOpts.Regression.SigmaNSQ = 'auto';
@@ -23,23 +26,40 @@ MetaOpts.MetaType = 'PCE';
 
 MetaOpts.Degree = 3;
 MetaOpts.ExpDesign.NSamples = size(X,1);
+
+%}
 IOpts.Inference.Data = X;
 IOpts.Copula.Type = 'Independent';
 IOpts.Marginals.Type = 'auto' ;
 myInput = uq_createInput(IOpts);
 
-end
+%end
 myMetamodel = uq_createModel(MetaOpts);
 uq_print(myMetamodel)
 uq_display(myMetamodel)
-%[AA ZZ] = uq_evalModel([0.001 2.5])
+
+
+YPCE = uq_evalModel(myMetamodel,X);
+uq_figure
+
+cmap = uq_colorOrder(2);
+uq_histogram(Y, 'FaceColor', cmap(1,:))
+hold on
+uq_histogram(YPCE, 'FaceColor', cmap(2,:))
+hold off
+
+xlabel('$\mathrm{Y}$')
+ylabel('Counts')
+uq_legend(...
+    {'True model response', 'PCE prediction'},...
+    'Location', 'northwest')
 
 SobolOpts.Type = 'Sensitivity';
 SobolOpts.Method = 'Sobol';
 SobolOpts.Sobol.Order = 1;
 
 
-
+%{
 if num ==1 
 IOpts.Inference.Data = X;
 IOpts.Copula.Type = 'Independent';
@@ -52,7 +72,7 @@ PCEOpts.Degree = 5;
 PCEOpts.ExpDesign.NSamples = size(X,1);
 myPCE = uq_createModel(PCEOpts);
 end
-
+%}
 mySobolAnalysis = uq_createAnalysis(SobolOpts)
 mySobolResultsPCE = mySobolAnalysis.Results;
 uq_print(mySobolAnalysis)
